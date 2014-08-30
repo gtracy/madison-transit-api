b350e680011177ba15e6e075c51f2f5b5e7915fc
gtracy <gtracy@gmail.com>
2014-08-29 20:46:27 -0500

    optimized the stop location porting loop

--- crawl/load_gtfs.py
+++ crawl/load_gtfs.py
@@ -18,31 +18,24 @@ from data_model import StopLocationLoader
 #
 class PortStopsHandler(webapp.RequestHandler):
     def get(self):
-    
-        # query the StopLocationLoader for all stops
-        q = db.GqlQuery("select * from StopLocationLoader")
-        stops = q.fetch(500)
-        offset = 500
-        while stops is not None and len(stops) > 0:
-            for s in stops:
-                # create a new task for each stop
-                task = Task(url='/gtfs/port/stop/task/', 
-                            params={'stopID':s.stopID,
-                                    'name':s.name,
-                                    'description':s.description,
-                                    'lat':str(s.lat),
-                                    'lon':str(s.lon),
-                                    'direction':s.direction,
-                                   })
-                task.add('crawler')
-                
-            # get the next bunch of stops from the query
-            stops = q.fetch(500,offset)
-            offset += 500
 
-        logging.debug('Finished spawning %s StopLocationLoader tasks!' % str(offset-500))
+        # query the StopLocationLoader for all stops
+        stops = StopLocationLoader.all()
+        for s in stops:
+            # create a new task for each stop
+            task = Task(url='/gtfs/port/stop/task/',
+                        params={'stopID':s.stopID,
+                                'name':s.name,
+                                'description':s.description,
+                                'lat':str(s.lat),
+                                'lon':str(s.lon),
+                                'direction':s.direction,
+                               })
+            task.add('crawler')
+
+        logging.debug('Finished spawning StopLocationLoader tasks!')
         self.response.out.write('done spawning porting tasks!')
-    
+
 ## end PortStopsHandler
 
 #
@@ -52,7 +45,7 @@ class PortStopTask(webapp.RequestHandler):
     def post(self):
         stop_list = []
         route_list = []
-        
+
         stopID      = self.request.get('stopID')
         if len(stopID) == 1:
             stopID = "000" + stopID
@@ -60,17 +53,17 @@ class PortStopTask(webapp.RequestHandler):
             stopID = "00" + stopID
         if len(stopID) == 3:
             stopID = "0" + stopID
-            
+
         name        = self.request.get('name')
         description = self.request.get('description')
         lat         = self.request.get('lat')
         lon         = self.request.get('lon')
         direction   = self.request.get('direction')
-        
+
         # if it doesn't, create a new one
         s = StopLocation()
         stop_template = s
-        
+
         s.stopID = stopID
         s.intersection = name.split('(')[0].rstrip()
         s.direction = direction
@@ -78,11 +71,11 @@ class PortStopTask(webapp.RequestHandler):
         s.location = GeoPt(lat,lon)
         s.update_location()
         stop_list.append(s)
-                
+
         # put the new stop in the datastore
         db.put(stop_list)
         logging.info('done updating stop locations for stopID %s' % stopID)
-        
+
         self.response.set_status(200)
 
 ## end PortStopTask
