e86f6dc3335188065a7475204c4828d9cfdb9668
Greg Tracy <gtracy@gmail.com>
2017-08-28 06:53:50 -0500

    turned on the persistent metrics for tracking API requests

--- api/v1/getarrivals.py
+++ api/v1/getarrivals.py
@@ -65,11 +65,6 @@ class MainHandler(webapp.RequestHandler):
                   task = Task(url='/map/task', params={'stopID':stopID})
                   task.add('eventlogger')
 
-              # stop statistics - DISABLED
-              # if( "kiosk" not in dev_key ):
-              #     task = Task(url='/stats/stop', params={'apikey':dev_key,'stop':stopID})
-              #     task.add('stats')
-
           else:
               # don't run these jobs during "off" hours
               #logging.debug('shunted... off hour request')
@@ -100,6 +95,11 @@ class MainHandler(webapp.RequestHandler):
       if api_utils.afterHours() is False and dev_key != 'uwkiosk9':
         stathat.apiTimeStat(config.STATHAT_API_GETARRIVALS_TIME_KEY,((time.time()-start)*1000))
         stathat.apiStatCount()
+
+        if( "kiosk" not in dev_key and "willycoop" not in dev_key ):
+            task = Task(url='/stats/stop', params={'apikey':dev_key,'stop':stopID})
+            task.add('stats')
+
 
 ## end RequestHandler
 
